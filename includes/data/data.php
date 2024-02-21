ee94b154422ec46a502354590bd3463a71f0c311
ariannacostanzo <costanzo.arianna@outlook.it>
2024-02-21 18:11:10 +0100

    implementing bonus 4

--- includes/data/data.php
+++ includes/data/data.php
@@ -4,10 +4,6 @@
 $numbers = ['1','2','3','4','5','6','7','8','9','0'];
 $letters_uppercase = range('A', 'Z');
 $letters_lowercase = range('a', 'z');
-$symbols = ['!', '.', '?', ',', '@', '#', '$', '%', '^', '&', '*'];
-
-//array che ha tutto
-$all_signs = [];
-$all_signs = array_merge($numbers , $letters_uppercase , $letters_lowercase , $symbols);
+$symbols = ['!', '.', '?', ',', '@', '#', '$', '%', '^', '&', '*', '$', '£', '/', '|', '|', '<', '>', '_', '-'];
 
 ?>
--- index.php
+++ index.php
@@ -8,15 +8,50 @@ include __DIR__ . '/includes/data/data.php';
 
 //lunghezza
 $length = $_GET['length'] ?? '';
+
 //ripetizione caratteri
 $repeat = $_GET['repetition'] ?? 'yes';
-var_dump($repeat);
-var_dump($length);
 
-//quando ho inviato il form
+//tipi di caratteri
+$c_letters = $_GET['letters'] ?? 'off';
+$c_numbers = $_GET['numbers'] ?? 'off';
+$c_symbols = $_GET['symbols'] ?? 'off';
+
+//scelgo cosa voglio che il mio array abbia
+$all_signs = [];
+
+//se tutto è off uso tutto
+if ($c_letters === 'off' && $c_numbers === 'off' && $c_symbols === 'off') {
+    $all_signs = array_merge($numbers , $letters_uppercase , $letters_lowercase , $symbols);
+}
+
+//se letters è on
+if ($c_letters === 'on') {
+    foreach($letters_lowercase as $letter_lower) {
+        array_push($all_signs, $letter_lower);
+    }
+    foreach($letters_uppercase as $letter_upper) {
+        array_push($all_signs, $letter_upper);
+    }
+} 
+//se numbers è on
+if ($c_numbers === 'on'){
+    foreach($numbers as $number) {
+        array_push($all_signs, $number);
+    }
+}
+//se symbols è on
+if ($c_symbols === 'on'){
+    foreach($symbols as $symbol) {
+        array_push($all_signs, $symbol);
+    }
+}
+
+
+// quando ho inviato il form
 if ($length) {
     //creo la password
-    $password = create_random_password($length, $numbers, $repeat);
+    $password = create_random_password($length, $all_signs, $repeat);
 
     session_start();
     $_SESSION['password'] = $password;
@@ -24,8 +59,6 @@ if ($length) {
     //mi sposto nella pagina di successo
     header('Location: success.php');
 }
-
-
 
 
 ?>
@@ -51,10 +84,12 @@ if ($length) {
         <!-- form -->
         <form class="password-container" action="" method="GET">
             
+            <!-- lunghezza password -->
             <div class="d-flex align-items-center justify-content-between my-4">
                 <label for="length">Lunghezza password:</label>
                 <input type="number" class="form-input" id="length" name="length" value="<?= $length ?>">
             </div>
+            <!-- ripetizione -->
             <div class="d-flex align-items-center justify-content-between my-4">
                 <label>Consenti ripetizioni di uno o più caratteri: </label>
                 <div class="d-flex align-items-center gap-2">
@@ -62,6 +97,24 @@ if ($length) {
                     <input type="radio" value="yes" id="rep-yes" name="repetition" <?= $repeat === 'yes' ? 'checked' : '' ?>>
                     <label for="rep-no">No</label>
                     <input type="radio" value="no" id="rep-no" name="repetition" <?= $repeat === 'no' ? 'checked' : '' ?>>
+                </div>
+            </div>
+            <!-- caratteri -->
+            <div class="d-flex align-items-center justify-content-between my-4">
+                <label>Scegli che tipo di caratteri vuoi: </label>
+                <div class="d-flex flex-column align-items-center gap-2">
+                    <div>
+                        <label for="letters">Lettere</label>
+                        <input type="checkbox" id="letters" name="letters" >
+                    </div>
+                    <div>
+                        <label for="numbers">Numeri</label>
+                        <input type="checkbox" id="numbers" name="numbers">
+                    </div>
+                    <div>
+                        <label for="symbols">Simboli</label>
+                        <input type="checkbox" id="symbols" name="symbols">
+                    </div>
                 </div>
             </div>
             
